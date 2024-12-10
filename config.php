a31357967449fe315c632c3886a24e2f88cd3533
Kjell Heinsen <ich@kjell-heinsen.de>
2024-12-10 23:13:19 +0100

    Add Blank Config Files

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+date_default_timezone_set('Europe/Berlin');
+define('DIR','https://yourdomain.tld/');
+define('DOCROOT', dirname(__FILE__));
+define('SITETITLE','Downloads');
+define('SESSION_PREFIX','PREFIX_');
+define('COPYRIGHT','YOURNAME');
+define('DefaultC','start');
+
+require_once 'sql_credits.php';
