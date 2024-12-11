5493d09bd82d201f97946b52ef83a7e4bc040cdc
Kjell Heinsen <ich@kjell-heinsen.de>
2024-12-11 17:29:44 +0100

    app/controller/install

---
+++ app/controllers/install.php
@@ -0,0 +1,20 @@
+<?php
+namespace downapitool\app\controllers;
+use downapitool\app\core\controller;
+use downapitool\app\helpers\url;
+
+
+class install extends controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        url::redirect('',303);
+    }
+}
+
